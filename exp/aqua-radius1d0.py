0f27c59b277e84d4782053bb20e32901e476b554
Nathanael Wong <natgeo.wong@outlook.com>
2020-12-02 00:13:16 -0500

    New set of experiments to run

---
+++ exp/aqua-radius1d0.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+import f90nml
+from isca import IscaCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE, GFDL_DATA
+
+NCORES = 32
+RESOLUTION = 'T85', 30
+
+cb = IscaCodeBase.from_directory(GFDL_BASE)
+cb.compile()
+
+earth_omega  = 7.292e-5
+earth_radius = 6376.0e3
+rscale = 1
+
+diag = DiagTable()
+diag.add_file('atmos_daily', 1, 'days', time_units='days')
+diag.add_field('dynamics', 'ps', time_avg=True)
+diag.add_field('dynamics', 'bk')
+diag.add_field('dynamics', 'pk')
+diag.add_field('dynamics', 'vor', time_avg=True)
+diag.add_field('dynamics', 'div', time_avg=True)
+diag.add_field('dynamics', 'ucomp', time_avg=True)
+diag.add_field('dynamics', 'vcomp', time_avg=True)
+diag.add_field('dynamics', 'temp', time_avg=True)
+
+spinup_fin = os.path.join(GFDL_DATA,'aqua/spinup/restarts/res0001.tar.gz')
+
+for oscale in np.arange(-2,1.1,0.2):
+
+    rfol = 'radius'   + '{:03.1f}'.format(rscale)
+    ofol = 'omegalog' + '{:03.1f}'.format(oscale)
+    exp = Experiment(os.path.join('aqua',rfol+'-'+ofol), codebase=cb)
+    exp.set_resolution(*RESOLUTION)
+    exp.clear_rundir()
+    exp.diag_table = diag
+    exp.namelist   = f90nml.read('namelistaqua.nml')
+    exp.update_namelist({
+        'constants_nml': {
+            'radius' : earth_radius * rscale,
+            'omega'  : earth_omega  * (10 ** oscale)
+        }
+    })
+
+    if __name__=="__main__":
+        exp.run(1, use_restart=True, restart_file=spinup_fin, num_cores=NCORES)
+        for i in range(2,4):
+            exp.run(i, num_cores=NCORES)
---
+++ exp/aqua-spinup.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import f90nml
+from isca import IscaCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE, GFDL_DATA
+
+NCORES = 32
+RESOLUTION = 'T85', 30
+
+cb = IscaCodeBase.from_directory(GFDL_BASE)
+cb.compile()
+
+earth_omega  = 7.292e-5
+earth_radius = 6376.0e3
+
+diag = DiagTable()
+diag.add_file('atmos_daily', 1, 'days', time_units='days')
+diag.add_field('dynamics', 'ps', time_avg=True)
+diag.add_field('dynamics', 'bk')
+diag.add_field('dynamics', 'pk')
+diag.add_field('dynamics', 'vor', time_avg=True)
+diag.add_field('dynamics', 'div', time_avg=True)
+diag.add_field('dynamics', 'ucomp', time_avg=True)
+diag.add_field('dynamics', 'vcomp', time_avg=True)
+diag.add_field('dynamics', 'temp', time_avg=True)
+
+exp = Experiment('aqua/spinup', codebase=cb)
+exp.set_resolution(*RESOLUTION)
+exp.clear_rundir()
+exp.diag_table = diag
+exp.namelist   = f90nml.read('namelistaqua.nml')
+
+exp.run(1, num_cores=NCORES, use_restart=False, overwrite_data=True)
